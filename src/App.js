1468852508dc86a32500bed530d73b03a997763c
Aziz Soorti <hies.aziz2019@gmail.com>
2022-11-05 07:06:16 -0700

    react router add

--- src/App.js
+++ src/App.js
@@ -1,34 +1,39 @@
 import Header from './components/Header'
 import Banner from './components/Banner';
 import Categories from './components/Categories';
-import Believe from './components/Believe';
+// import Believe from './components/Believe';
 import Features from './components/Features';
 import Event from './components/Event';
-import Card_feature from './components/Card_feature';
+// import Card_feature from './components/Card_feature';
 import Price from './components/Price';
 import Questions from './components/Questions';
-import Download from './components/Download';
+// import Download from './components/Download';
 import Footer from './components/Footer';
 import './App.css';
-
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div style={{ overflowX: "hidden" }}>
 
       <Header />
-      <Banner />
-      <Categories />
-      <Believe />
-      <Features />
-      <Event />
-      <Card_feature />
-      <Price />
-      <Questions />
-      <Download />
+
+      <Routes>
+
+        {/* <Route path="/" element={<Header />} /> */}
+        <Route path="banner" element={<Banner />} />
+        <Route path="categories" element={<Categories />} />
+        {/* <Route path="believe" element={<Believe />} /> */}
+        <Route path="features" element={<Features />} />
+        <Route path="event" element={<Event />} />
+        {/* <Route path="card_feature" element={<Card_feature />} /> */}
+        <Route path="price" element={<Price />} />
+        <Route path="questions" element={<Questions />} />
+        {/* <Route path="download" element={<Download />} /> */}
+        {/* <Route path="footer" element={<Footer />} /> */}
+
+      </Routes>
       <Footer />
-
-
     </div>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,69 +1,80 @@
+import { Link } from "react-router-dom";
 function Header() {
   return (
-  
-   
-  <section id="header" className="container-fluid bg-white fixed-top">
-  <div className="container">
-    <div className="row bg-white ">
-      <div className="col-6 ">
-        <img className="py-3" src="./images/logo.webp" alt="" />
-      </div>
-      <div className="col-6 ">
-        <nav className="navbar navbar-expand-lg  ">
-          <div className="container-fluid">
-            <button
-              className="navbar-toggler ms-auto"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarNav"
-              aria-controls="navbarNav"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon" />
-            </button>
-            <div className="collapse navbar-collapse " id="navbarNav">
-              <ul className="navbar-nav ms-auto  ">
-                <li className="nav-item ">
-                  <a className="nav-link  fw-bold px-3 " href="#header">
-                    HOME
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link fw-bold px-3  " href="#categories">
-                    {" "}
-                    SERVICE
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link fw-bold px-3  " href="#features">
-                    {" "}
-                    FEATURES
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link fw-bold px-3  " href="#price">
-                    PRICING
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link fw-bold px-3  " href="#questions">
-                    FAQ
-                  </a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link fw-bold px-3  " href="#event">
-                    PAGES
-                  </a>
-                </li>
-              </ul>
-            </div>
+
+    <section id="header" className="container-fluid bg-white fixed-top">
+      <div className="container">
+        <div className="row bg-white ">
+          <div className="col-6 ">
+            <img className="py-3" src="./images/logo.webp" alt="" />
           </div>
-        </nav>
+          <div className="col-6 ">
+            <nav className="navbar navbar-expand-lg  ">
+              <div className="container-fluid">
+                <button
+                  className="navbar-toggler ms-auto"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#navbarNav"
+                  aria-controls="navbarNav"
+                  aria-expanded="false"
+                  aria-label="Toggle navigation"
+                >
+                  <span className="navbar-toggler-icon" />
+                </button>
+                <div className="collapse navbar-collapse " id="navbarNav">
+                  <ul className="navbar-nav ms-auto  ">
+                    <li className="nav-item">
+
+                      <Link to="banner" className="nav-link  fw-bold px-3 " >  HOME </Link>
+
+
+                    </li>
+
+                    <li className="nav-item">
+
+                      <Link to="categories" className="nav-link fw-bold px-3  " >
+                        {" "}
+                        SERVICE
+                      </Link>
+                    </li>
+                    <li className="nav-item">
+
+                      <Link to="features" className="nav-link fw-bold px-3  ">
+                        {" "}
+                        FEATURES
+                      </Link>
+
+                    </li>
+                    <li className="nav-item">
+
+                      <Link to="price" className="nav-link fw-bold px-3  ">
+                        PRICING
+                      </Link>
+
+                    </li>
+                    <li className="nav-item">
+
+                      <Link to="questions" className="nav-link fw-bold px-3  ">
+                        FAQ
+                      </Link>
+
+                    </li>
+                    <li className="nav-item">
+
+                      <Link to="event" className="nav-link fw-bold px-3  " href="#event">
+                        PAGES
+                      </Link>
+
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </nav>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-</section>
+    </section>
   )
 }
 
